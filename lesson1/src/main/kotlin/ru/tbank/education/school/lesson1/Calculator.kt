b1e7093ec2a7735d6de3a1f29d641107302b7d45
Krupnov <pavelkm10@gmail.com>
2024-09-14 15:09:10 +0300

    solving

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,10 +1,34 @@
 package ru.tbank.education.school.lesson1
 
+enum class OperationType {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+}
+
 /**
  * Метод для вычисления простых арифметических операций.
  */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO() 
+fun calculate(
+    a: Double,
+    b: Double,
+    operation: OperationType,
+): Double? {
+    when (operation) {
+        OperationType.ADD -> return a + b
+        OperationType.SUBTRACT -> return a - b
+        OperationType.MULTIPLY -> return a * b
+        OperationType.DIVIDE -> {
+            if (b == 0.0) {
+                throw RuntimeException("Division by zero")
+            }
+            return a / b
+        }
+        else -> {
+            return null
+        }
+    }
 }
 
 /**
@@ -14,5 +38,18 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val expression = split(" ")
+    var a = expression[0].toDouble()
+    var b = expression[2].toDouble()
+    return when (expression[1]) {
+        "+" -> calculate(a, b, OperationType.ADD)
+        "-" -> calculate(a, b, OperationType.SUBTRACT)
+        "*" -> calculate(a, b, OperationType.MULTIPLY)
+        "/" -> calculate(a, b, OperationType.DIVIDE)
+        else -> throw RuntimeException("unknown operation: ${expression[1]}")
+    }
+}
+
+fun main(args: Array<String>) {
+    println("1 % 10".calculate())
 }
--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,4 +1,8 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
+    var numbers = arrayOf(1, 2, 3, 4, 5, 6)
+    for (i in 0..5) {
+        println(numbers[i])
+    }
 }
