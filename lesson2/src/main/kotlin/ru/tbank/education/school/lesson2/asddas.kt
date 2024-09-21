125ef5ce4405bf524fa40d57c080dc15f778cd1f
bolrob <belkavas78@mail.ru>
2024-09-21 11:02:00 +0300

    Беличенко Василий

---
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/asddas.kt
@@ -0,0 +1,12 @@
+package ru.tbank.education.school.lesson2
+
+fun main() {
+    val a = arrayOf(2, 3, 4, 3, 6, 2, 3, 3, 3)
+    for (i in a){
+        var count = a.count {it == i}
+        if (count > a.size / 2) {
+            print(i)
+            break
+        }
+    }
+}
