45f61e83054d2826291ad72e9c5b67fe101ad824
ilyat <ilyatirex@gmail.com>
2024-09-21 11:02:03 +0300

    first exercise

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
@@ -1,11 +1,16 @@
 package ru.tbank.education.school.lesson2
 
 fun main() {
-    val list = DemoList.createList()
-    println("Original list: $list")
-    TODO()
-
-    val map = DemoMap.createMap()
-    println("Original map: $map")
-    TODO()
+    var v = arrayOf(2, 3, 4, 3, 6, 2, 3 ,3)
+    var n = v.size / 2
+    for (i in v) {
+        var cnt = 0
+        for (j in v) {
+            if (i == j) ++cnt
+        }
+        if (cnt >= n) {
+            println(i)
+            break
+        }
+    }
 }
