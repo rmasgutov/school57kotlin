61b1ac48abc46a4f146e62e45c2bb5b27639a117
Konstantin Neruxov <k.neruxov@gmail.com>
2024-09-21 13:00:30 +0300

    feat: implemented DemoMap and DemoList

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -1,23 +1,42 @@
 package ru.tbank.education.school.lesson2
 
+import kotlin.random.Random
+
 object DemoMap {
+
     fun createMap(): Map<String, Int> {
-        TODO()
+        val result = mutableMapOf<String, Int>()
+
+        for (i in 1..5) {
+            val length = Random.nextInt(5, 11)
+            result[randomString(length)] = Random.nextInt(1, 101)
+        }
+
+        return result
     }
 
     fun maxValue(): Int {
-        TODO()
+        return createMap().values.maxOrNull() ?: 0
     }
 
     fun keyForMaxValue(): String {
-        TODO()
+        return createMap().maxByOrNull { it.value }!!.key
     }
 
     fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+        return createMap().toList().sortedByDescending { (_, value) -> value }.toMap()
     }
 
     fun filterOddValues(): Map<String, Int> {
-        TODO()
+        return createMap().filter { it.value % 2 != 0 }
     }
+
+    // source: https://stackoverflow.com/questions/46943860/idiomatic-way-to-generate-a-random-alphanumeric-string-in-kotlin
+    private fun randomString(length: Int): String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return (1..length)
+            .map { allowedChars.random() }
+            .joinToString("")
+    }
+
 }
