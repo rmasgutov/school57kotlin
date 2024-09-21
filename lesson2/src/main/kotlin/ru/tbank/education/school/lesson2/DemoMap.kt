90786689bed8f997ec27b50998882667261064c0
alex_cu_1 <alexfaleev2009@gmail.com>
2024-09-21 18:08:27 +0300

    ехехеххехе

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -1,22 +1,31 @@
 package ru.tbank.education.school.lesson2
-
+import kotlin.random.Random
 object DemoMap {
+    private val map: Map<String, Int> = createMap()
+    fun getRandomString(minLength: Int, maxLength: Int): String {
+        val allowedChars = ('a'..'z') + ('A'..'Z')
+        return (1..Random.nextInt(minLength, maxLength + 1)).map { allowedChars.random() }.joinToString("")
+    }
     fun createMap(): Map<String, Int> {
-        TODO()
+        return mapOf(
+            getRandomString(5, 10) to Random.nextInt(1, 101),
+            getRandomString(5, 10) to Random.nextInt(1, 101),
+            getRandomString(5, 10) to Random.nextInt(1, 101),
+            getRandomString(5, 10) to Random.nextInt(1, 101),
+            getRandomString(5, 10) to Random.nextInt(1, 101)
+        )
     }
     fun maxValue(): Int {
-        TODO()
+        return createMap().values.max()
     }
-
     fun keyForMaxValue(): String {
-        TODO()
+        return createMap().maxByOrNull { it.value }!!.key
     }
-
     fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+        return map.toList().sortedByDescending { it.second }.toMap()
+    }
+    fun filterOddValues(): Map<String, Int> {
+        return createMap().filterValues { it % 2 == 1 }
     }
 
-    fun filterOddValues(): Map<String, Int> {
-        TODO()
-    }
 }
