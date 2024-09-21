7de3a7cf35325a69b1a08080990fdf8cae61c5d7
Dan <76772649+physcorgi@users.noreply.github.com>
2024-09-21 10:52:19 +0300

    тест

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,5 +1,23 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    println(1)
+    val a = arrayOf(2, 3, 4, 3, 6, 2, 3, 3,3,3,3,3)
+    var candidate = a[0]
+    var count = 0
+
+    for (num in a) {
+        if (count == 0) {
+            candidate = num
+        }
+        count += if (num == candidate) 1 else -1
+    }
+    count = 0
+    for (num in a) {
+        if (num == candidate) {
+            count++
+        }
+    }
+    if (count > a.size / 2) {
+        println(candidate)
+    }
 }
