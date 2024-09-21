21eb7cd7c4ac51f958bdd17431c0cfdcc20fcdd8
Maria Nepomyaschaya <marianepom555@gmail.com>
2024-09-21 10:56:41 +0300

    я тренируюсь

---
+++ lesson2/src/main/kotlin/exersize.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var arr = listOf(1, 2, 2, 2, 2, 3, 4)
+    var n = arr.size
+    arr = arr.sorted()
+    var x = -1
+    var k = 0
+    for (i in arr) {
+        if (i == x) {
+            k++
+        } else {
+            x = i
+            k = 1
+        }
+        if (k > n / 2) {
+            println(x)
+            break
+        }
+    }
+}
