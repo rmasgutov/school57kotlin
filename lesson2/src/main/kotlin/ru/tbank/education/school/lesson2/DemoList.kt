93e0dda45449043b239867d27889e74d600d0721
Алиса Ласкина <alice.laskina@yandex.ru>
2024-09-21 14:20:26 +0300

    Ласкина Алиса, задание на уроке

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
@@ -2,26 +2,31 @@ package ru.tbank.education.school.lesson2
 
 object DemoList {
     fun createList(): List<Int> {
-        TODO()
+        var data = listOf(1, 5, 8, 30, 27)
+        return data
     }
 
-    fun sumList(): Int {
-        TODO()
+    fun sumList(data: List<Int>): Int {
+        return data.sum()
     }
 
-    fun sumEvenList(): Int {
-        TODO()
+    fun sumEvenList(data: List<Int>): Int {
+        var res = 0
+        for (i in 0..data.size) {
+            res = res + data[i]
+        }
+        return res
     }
 
-    fun multiplyList(): List<Int> {
-        TODO()
+    fun multiplyList(data: List<Int>): List<Int> {
+        return data.map { it * 2 }
     }
 
-    fun maxElement(): Int {
-        TODO()
+    fun maxElement(data: List<Int>): Int {
+        return data.max()
     }
 
-    fun sortDesc(): List<Int> {
-        TODO()
+    fun sortDesc(data: List<Int>): List<Int> {
+        return data.sorted().reversed()
     }
 }
--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -2,22 +2,28 @@ package ru.tbank.education.school.lesson2
 
 object DemoMap {
     fun createMap(): Map<String, Int> {
-        TODO()
+        var data = mapOf("sdjfahgmj" to 3, "dfjhs" to 6, "fgh" to 90, "h" to 35, "hjk" to 56)
+        return data
     }
 
-    fun maxValue(): Int {
-        TODO()
+    fun maxValue(data: Map<String, Int>): Int {
+        return data.values.max()
     }
 
-    fun keyForMaxValue(): String {
-        TODO()
+    fun keyForMaxValue(data: Map<String, Int>): String {
+        var res = data.values.max()
+        for (i in data.keys) {
+            if (data[i] == res) {
+                return i
+            }
+        }
     }
 
-    fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+    fun sortByValueDesc(data: Map<String, Int>): Map<String, Int> {
+        return data.toSortedMap()
     }
 
-    fun filterOddValues(): Map<String, Int> {
-        TODO()
+    fun filterOddValues(data: Map<String, Int>): Map<String, Int> {
+
     }
 }
