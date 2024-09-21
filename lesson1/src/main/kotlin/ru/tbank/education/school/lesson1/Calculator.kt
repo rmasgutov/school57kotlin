d6084fa1b9060d391398f5e791588600fb172380
mmatvey <matvei.minervini@gmail.com>
2024-09-21 10:28:08 +0300

    чето сделал

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -4,7 +4,14 @@ package ru.tbank.education.school.lesson1
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    val ans = when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> if (b == 0.0) throw Exception("Деление на ноль") else a / b
+    }
+    println(ans)
+    return ans
 }
 
 /**
@@ -14,5 +21,16 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val nums = split(" ")
+    if (nums.size != 3) return null
+    val a = nums[0].toBigDecimal()
+    val b = nums[2].toBigDecimal()
+    val operation = when(nums[1]) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> null
+    }
+    return ru.tbank.education.school.lesson1.calculate(a, b)
 }
