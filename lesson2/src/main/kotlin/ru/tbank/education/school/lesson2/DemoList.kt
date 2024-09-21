00bcac5f957138f303116ae883f6df91437f593e
Maria Nepomyaschaya <marianepom555@gmail.com>
2024-09-21 13:13:59 +0300

    exersize

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
@@ -1,27 +1,46 @@
 package ru.tbank.education.school.lesson2
 
+import kotlin.random.Random
+
 object DemoList {
     fun createList(): List<Int> {
-        TODO()
+        val randomList = List(5) { Random.nextInt(1, 100) }
+        return randomList
     }
 
-    fun sumList(): Int {
-        TODO()
+    fun sumList(a : List<Int>): Int {
+        var sum = 0
+        for (i in a) {
+            sum += i
+        }
+        return sum
     }
 
-    fun sumEvenList(): Int {
-        TODO()
+    fun sumEvenList(a : List<Int>): Int {
+        var sum = 0
+        val n = a.size
+        for (i in 1..n) {
+            if (i % 2 == 0) sum += a[i - 1]
+        }
+        return sum
     }
 
-    fun multiplyList(): List<Int> {
-        TODO()
+    fun multiplyList(a : List <Int>): List<Int> {
+        a.map{it * 2}
+        return a
     }
 
-    fun maxElement(): Int {
-        TODO()
+    fun maxElement(a : List <Int>): Int {
+        var maxx = 0
+        for (i in a) {
+            if (i > maxx) maxx = i
+        }
+        return maxx
     }
 
-    fun sortDesc(): List<Int> {
-        TODO()
+    fun sortDesc(a : List <Int>): List<Int> {
+        a.sorted()
+        a.reversed()
+        return a
     }
 }
