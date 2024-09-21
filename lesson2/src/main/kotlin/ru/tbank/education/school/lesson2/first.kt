05e3537e0ab6f37cf59215f4f5aec10ef0eec90b
Slava-ne-Merlow <SlavaKushch@yandex.ru>
2024-09-21 11:13:28 +0300

    first exercise

---
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/first.kt
@@ -0,0 +1,23 @@
+package ru.tbank.education.school.lesson2
+
+class first {
+
+
+    fun main() {
+        val lis = listOf(2, 3, 4, 3, 6, 2, 3 ,3)
+        val leng = lis.size / 2
+        for (n in lis){
+            var c = 0
+            for (j in lis){
+                if (j == n) {
+                    c += 1
+                }
+            }
+            if (c == leng) {
+                println(n)
+                break
+            }
+        }
+    }
+
+}
