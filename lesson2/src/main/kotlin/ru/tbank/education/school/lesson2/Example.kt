c2fac31927d04f3b47a787d7511038bb13a970ab
Konstantin Neruxov <k.neruxov@gmail.com>
2024-09-21 11:43:09 +0300

    feat: implement sort in Example.kt

---
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Example.kt
@@ -0,0 +1,8 @@
+package ru.tbank.education.school.lesson2
+
+/**
+ * @author <a href="https://github.com/Neruxov">Neruxov</a>
+ */
+fun sort(list: List<Int>): List<Int> {
+    return list.sorted()
+}
