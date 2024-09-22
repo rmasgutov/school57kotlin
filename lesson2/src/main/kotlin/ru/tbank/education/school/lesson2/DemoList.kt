7532c3f9d213013abd89631f430035ddb0e38ae9
Никорук Михаил <nmv0808@mail.ru>
2024-09-22 22:06:17 +0300

    aaa

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
@@ -1,27 +1,36 @@
 package ru.tbank.education.school.lesson2
 
+import kotlin.random.Random
 object DemoList {
     fun createList(): List<Int> {
         TODO()
+        return List(5) {Random.nextInt(1, 101)}
     }
 
     fun sumList(): Int {
         TODO()
+        return createList().sum()
     }
 
     fun sumEvenList(): Int {
         TODO()
+        val data = createList()
+        val evenData = data.filter { it % 2 == 0 }
+        return evenData.sum()
     }
 
     fun multiplyList(): List<Int> {
         TODO()
+        return createList().map { it.times(other = 2) }
     }
 
     fun maxElement(): Int {
         TODO()
+        return createList().max()
     }
 
     fun sortDesc(): List<Int> {
         TODO()
+        return createList().sortedDescending()
     }
 }
