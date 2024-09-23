27fd68bcf742c90f264ebf8e0c16262f1f1fd951
bolrob <belkavas78@mail.ru>
2024-09-23 18:30:42 +0300

    Доделал операции с мапом

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -1,23 +1,52 @@
 package ru.tbank.education.school.lesson2
 
+import kotlin.random.Random
+
+fun getRandomString(length: Int) : String {
+    val charset = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz0123456789"
+    return (1..length)
+        .map { charset.random() }
+        .joinToString("")
+}
+
 object DemoMap {
     fun createMap(): Map<String, Int> {
-        TODO()
+        return mutableMapOf(getRandomString(Random.nextInt(5, 10)) to Random.nextInt(), getRandomString(Random.nextInt(1, 100)) to Random.nextInt(), getRandomString(Random.nextInt(5, 100)) to Random.nextInt(), getRandomString(Random.nextInt(5, 100)) to Random.nextInt(), getRandomString(Random.nextInt(5, 100)) to Random.nextInt())
     }
-
+    fun printMap() {
+        val a = createMap()
+        for (item in a) {
+            println("${item.key}: ${item.value}")
+        }
+    }
     fun maxValue(): Int {
-        TODO()
+        val a = createMap()
+        var b = 0
+        for (item in a) {
+            if (item.value > b) b = item.value
+        }
+        return b
     }
 
     fun keyForMaxValue(): String {
-        TODO()
+        val a = createMap()
+        var b = 0
+        var c = "1"
+        for (item in a) {
+            if (item.value > b) {
+                b = item.value
+                c = item.key
+            }
+        }
+        return c
     }
 
     fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+        val a = createMap().toList()
+        return a.sortedBy { it.second }.toMap()
     }
 
     fun filterOddValues(): Map<String, Int> {
-        TODO()
+        return createMap().filterValues { it % 2 == 0 }
     }
 }
