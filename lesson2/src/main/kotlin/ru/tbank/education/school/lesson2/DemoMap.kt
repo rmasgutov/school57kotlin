ff0b77145381ddcf6ea219d2ba4e7eb012e2163f
Slava-ne-Merlow <SlavaKushch@yandex.ru>
2024-09-25 19:33:27 +0300

    Конец работы DemoMap и DemoList

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -1,23 +1,49 @@
 package ru.tbank.education.school.lesson2
+import kotlin.random.Random
 
 object DemoMap {
     fun createMap(): Map<String, Int> {
-        TODO()
+        return mapOf(Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100))
     }
 
-    fun maxValue(): Int {
-        TODO()
+    fun maxValue(map:Map<String, Int>): Int {
+        return map.values.max()
     }
 
-    fun keyForMaxValue(): String {
-        TODO()
+    fun keyForMaxValue(map:Map<String, Int>): String {
+        val m = maxValue(map)
+        for (i in map.keys){
+            if (map[i] == m) return i
+        }
+        return ""
     }
 
-    fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+    fun sortByValueDesc(map:Map<String, Int>): Map<String, Int> {
+
+        return map.toList().sortedByDescending { (_, value) -> value }.toMap()
     }
 
-    fun filterOddValues(): Map<String, Int> {
-        TODO()
+    fun filterOddValues(map:Map<String, Int>): Map<String, Int> {
+        return map.filter { (_, value) -> value % 2 == 0 }
+    }
+}
+fun main() {
+    val a = DemoMap.createMap()
+    for ((key, value )in a){
+        println("$key : $value")
+    }
+    println(DemoMap.maxValue(a))
+    println(DemoMap.keyForMaxValue(a))
+    println(DemoMap.sortByValueDesc(a))
+    for ((key, value )in DemoMap.sortByValueDesc(a)){
+        println("$key : $value")
+    }
+    println(DemoMap.filterOddValues(a))
+    for ((key, value )in DemoMap.filterOddValues(a)){
+        println("$key : $value")
     }
 }
--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
@@ -1,11 +1,28 @@
 package ru.tbank.education.school.lesson2
 
-fun main() {
-    val list = DemoList.createList()
-    println("Original list: $list")
-    TODO()
 
-    val map = DemoMap.createMap()
-    println("Original map: $map")
-    TODO()
+fun main(){
+    println("DemoList")
+    var a = DemoList.createList()
+    println(a)
+    println(DemoList.sumList(a))
+    println(DemoList.sumEvenList(a))
+    println(DemoList.multiplyList(a))
+    println(DemoList.maxElement(a))
+    println(DemoList.sortDesc(a))
+    println("DemoMap")
+    val b = DemoMap.createMap()
+    for ((key, value )in b){
+        println("$key : $value")
+    }
+    println(DemoMap.maxValue(b))
+    println(DemoMap.keyForMaxValue(b))
+    println(DemoMap.sortByValueDesc(b))
+    for ((key, value )in DemoMap.sortByValueDesc(b)){
+        println("$key : $value")
+    }
+    println(DemoMap.filterOddValues(b))
+    for ((key, value )in DemoMap.filterOddValues(b)){
+        println("$key : $value")
+    }
 }
