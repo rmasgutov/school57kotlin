1a0eb0b00e38861e83ef81d53e8e9713ddfb0815
ilyat <ilyatirex@gmail.com>
2024-09-25 16:32:06 +0300

    листик и мапа

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -1,23 +1,35 @@
 package ru.tbank.education.school.lesson2
+import kotlin.random.Random
 
 object DemoMap {
     fun createMap(): Map<String, Int> {
-        TODO()
+        return mapOf(Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100),
+            Random.nextInt(10000, 1000000000).toString() to Random.nextInt(1, 100))
+    }
+
+    fun allVelue() {
+        var a = createMap()
+        for (item in a)
+            println("${item.key}: ${item.value}")
+
     }
 
     fun maxValue(): Int {
-        TODO()
+        return createMap().values.max()
     }
 
     fun keyForMaxValue(): String {
-        TODO()
+        return createMap().maxByOrNull { it.value }!!.key
     }
 
     fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+        return createMap().toList().sortedByDescending { it.second }.toMap()
     }
 
     fun filterOddValues(): Map<String, Int> {
-        TODO()
+        return createMap().filterValues { it % 2 == 1 }
     }
 }
