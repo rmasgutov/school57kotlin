c2901a3c8a27600397132e98bc70963d75c3e28a
rafbig <raf.big8@gmail.com>
2024-09-25 20:37:56 +0300

    Биглов Рафаэль

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
@@ -5,32 +5,32 @@ object DemoList {
          return listOf(1, 6, 7, 89, 45 ,67 , 9, 11, 34, 53, 22)
     }
 
-    fun sumList(): Int {
+    fun sumList(a: List<Int>): Int {
         var counter = 0
-        for (num in createList()) {
-            counter = counter + num
+        for (num in a) {
+            counter += num
         }
         return counter
     }
 
-    fun sumEvenList(): Int {
+    fun sumEvenList(a: List<Int>): Int {
         var even_counter = 0
-        for (num in createList()) {
+        for (num in a) {
             if (num % 2 == 0) {
-                even_counter = even_counter + num
+                even_counter += num
             }
         }
         return even_counter
     }
 
-    fun multiplyList(): List<Int> {
-        val result = createList().map{it *2}
+    fun multiplyList(a: List<Int>): List<Int> {
+        val result = a.map{it *2}
         return result
     }
 
-    fun maxElement(): Int {
+    fun maxElement(a: List<Int>): Int {
         var maxx = 0
-        for (num in createList()) {
+        for (num in a) {
             if (num > maxx) {
                 maxx = num
             }
@@ -39,6 +39,6 @@ object DemoList {
     }
 
     fun sortDesc(): List<Int> {
-        return createList().sorted().reversed()
+        return createList().sortedDescending()
     }
 }
--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -1,26 +1,38 @@
 package ru.tbank.education.school.lesson2
 
-import kotlin.random.Random
-
 object DemoMap {
     private var map = mutableMapOf<String, Int>()
     fun createMap(): Map<String, Int> {
-        TODO()
+        return mapOf("a" to 1, "b" to 6, "c" to 7, "d" to 89, "e" to 45, "f" to 67, "g" to 9, "k" to 11, "l" to 34, "m" to 53, "n" to 22)
     }
 
-    fun maxValue(): Int {
-        TODO()
+    fun maxValue(b: Map<String, Int>): Int {
+        var maxx = 0
+        for ((key, value) in b) {
+            if (value > maxx) {
+                maxx = value
+            }
+        }
+        return maxx
     }
 
-    fun keyForMaxValue(): String {
-        TODO()
+    fun keyForMaxValue(b: Map<String, Int>): String {
+        var maxx = 0
+        var s = ""
+        for ((key, value) in b) {
+            if (value > maxx) {
+                maxx = value
+                s = key
+            }
+        }
+        return s
     }
 
-    fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+    fun sortByValueDesc(b: Map<String, Int>): Map<String, Int> {
+        return b.toList().sortedByDescending { it.second }.toMap()
     }
 
-    fun filterOddValues(): Map<String, Int> {
-        TODO()
+    fun filterOddValues(b: Map<String, Int>): Map<String, Int> {
+        return b.filterValues {it % 2 == 0}
     }
 }
