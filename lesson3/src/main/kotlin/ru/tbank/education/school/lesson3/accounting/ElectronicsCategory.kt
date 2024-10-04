3085fa9ddbda228ecf423fff22a88ce997d238a7
maplerain42 <kolesnikova.09.04.09@gmail.com>
2024-10-04 20:14:29 +0300

    3 урок Колесникова Алёна

---
+++ lesson3/src/main/kotlin/ru/tbank/education/school/lesson3/accounting/ElectronicsCategory.kt
@@ -0,0 +1,14 @@
+package ru.tbank.education.school.lesson3.accounting
+
+class ElectronicsCategory(name:String, products:MutableList<Product>):Category(name, products){
+    override fun sell(c:Int, ind:Int):Boolean{
+        if(this.products[ind].count>1){
+            this.products[ind].count -= 1
+        }
+        else{
+            this.products[ind].count = 0
+            return true
+        }
+        return false
+    }
+}
