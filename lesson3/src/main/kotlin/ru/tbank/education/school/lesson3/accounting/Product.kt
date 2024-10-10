dca93e977195d9717c48e4c1b6f9c0ac5ab23907
Ashot_27 <zvyagintsev208@gmail.com>
2024-10-10 17:47:04 +0300

    hometask

---
+++ lesson3/src/main/kotlin/ru/tbank/education/school/lesson3/accounting/Product.kt
@@ -0,0 +1,36 @@
+package ru.tbank.education.school.lesson3.accounting
+
+
+data class Product(
+    var name: String,
+    var price: Int,
+    var count: Int
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other is Product) {
+            return name == other.name
+        }
+        return false
+    }
+
+
+    operator fun plus(other: Product): Product {
+        if (this == other) {
+            return Product(this.name, this.price, this.count + other.count)
+        }
+        return this
+    }
+
+
+    operator fun minus(other: Product): Product {
+        if (this == other) {
+            return Product(this.name, this.price, this.count - other.count)
+        }
+        return this
+    }
+
+
+    operator fun times(other: Int): Product {
+        return Product(this.name, this.price, this.count * other)
+    }
+}
