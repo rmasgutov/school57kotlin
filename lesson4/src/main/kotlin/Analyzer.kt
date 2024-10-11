6c8e209b2f70fda476239b78ef272f19fede86e7
bolrob <belkavas78@mail.ru>
2024-10-12 00:31:15 +0300

    Урок 4 Беличенко Василий

--- lesson4/src/main/kotlin/Analyzer.kt
+++ lesson4/src/main/kotlin/Analyzer.kt
@@ -1,26 +1,48 @@
 package ru.tbank.education.school
 
-/**
- * Класс для анализа содержимого файла.
- */
-object Analyzer {
-    /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java IO.
-     * @param source путь до исходного файла.
-     * @param target путь до файла с результатами.
-     */
-    fun processFileIO(source: String, target: String) {
-        TODO()
-    }
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 
-    /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java NIO.
-     * @param source путь до исходного файла.
-     * @param target путь до файла с результатами.
-     */
+object Analyzer {
+    fun processFileIO(source: String, target: String) {
+        try {
+            val fin = File(source)
+            val fout = File(target)
+            val lines = fin.readLines()
+            val words = lines.flatMap { it.split(" ").map { word -> word.lowercase()}}
+            val meanWordsLen: Int
+            if (words.isNotEmpty()) meanWordsLen = words.sumOf { it.length } / words.size
+            else meanWordsLen = 0
+            fout.writeText("Общее количество строк: ${lines.size}\nОбщее количество слов: ${words.size}\n" +
+                    "Уникальные слова: ${words.toSet().size}\nСредняя длина слов: ${meanWordsLen}\n")
+        }
+        catch (e: FileNotFoundException) {
+            println("Такого файла не существует: ${e.message}")
+        }
+        catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        }
+    }
     fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            val fin = Paths.get(source)
+            val fout = Paths.get(target)
+            val lines = Files.readAllLines(fin)
+            val words = lines.flatMap { it.split(" ").map { word -> word.lowercase()}}
+            val meanWordsLen: Int
+            if (words.isNotEmpty()) meanWordsLen = words.sumOf { it.length } / words.size
+            else meanWordsLen = 0
+            Files.write(fout, "Общее количество строк: ${lines.size}\nОбщее количество слов: ${words.size}\nУникальные слова: ${words.toSet().size}\nСредняя длина слов: ${meanWordsLen}\n".toByteArray())
+        }
+        catch (e: FileNotFoundException) {
+            println("Такого файла не существует: ${e.message}")
+        }
+        catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        }
     }
 }
