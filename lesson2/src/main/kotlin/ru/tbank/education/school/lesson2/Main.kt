ca06d6bc2ea937d0e84369c5faa9f68160dc0750
ilyat <ilyatirex@gmail.com>
2024-10-11 13:51:03 +0300

    13:50.11.10.24

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
@@ -1,6 +1,7 @@
 package ru.tbank.education.school.lesson2
 //  maincraft
 
+
 import kotlin.random.Random
 
 
@@ -11,18 +12,55 @@ open class Character(
     val NPS:String,
     val isBot:Boolean
 ) {
-    fun say() {println("Hi")}
+    open fun say() {println("Hi")}
 }
+
+
 
 class Monstr(
+    x:Int, y:Int, name:String, NPS:String, isBot:Boolean
 
-) {
+) : Character(x, y, name, NPS, isBot = true), Moveable{
 
+    constructor(name: String) : this(
+        Random.nextFloat() * 100,
+        Random.nextFloat() * 100,
+        name
+    )
+    override fun move(x_2:Int, y_2:Int) {
+        x += x_2
+        y += y_2
+        println("I'm moving to x=$x, y=$y")
+    }
 }
 
 
+class Hero(
+    x:Int, y:Int, name:String, NPS:String, isBot:Boolean,
+    var hasBoots: Boolean
+
+) : Character(x, y, name, NPS, isBot = false), Moveable {
+    override fun say() {println("I'm a hero! $name")}
+
+    constructor(name: String, hasBoots: Boolean) : this(
+        Random.nextFloat() * 100,
+        Random.nextFloat() * 100,
+        name,
+        hasBoots
+    )
+
+    override fun move(x_2:Int, y_2:Int) {
+        var move_flag = 0
+        if (hasBoots) {
+            move_flag = 2
+        } else {move_flag = 1}
+        x += x_2 * move_flag
+        y += y_2 * move_flag
+        println("I'm moving to x=$x, y=$y")
+    }
+}
 
 
-fun main() {
-
+interface Moveable {
+    fun move(dx: Int, dy: Int)
 }
