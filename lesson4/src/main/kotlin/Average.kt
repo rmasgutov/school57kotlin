b001aae97fff543d6aa6e58d2992476b68e38c97
Konstantin Neruxov <k.neruxov@gmail.com>
2024-10-11 23:39:04 +0300

    feat: completed Analyzer and Average tasks

--- lesson4/src/main/kotlin/Average.kt
+++ lesson4/src/main/kotlin/Average.kt
@@ -1,9 +1,16 @@
 package ru.tbank.education.school
 
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
 /**
  * Класс для вычисления среднего арифметического.
  */
 object Average {
+
     /**
      * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
      * Обработка выполняется через Java IO.
@@ -11,7 +18,24 @@ object Average {
      * @param target путь до файла с результатами.
      */
     fun processFileIO(source: String, target: String) {
-        TODO()
+        try {
+            val input = File(source)
+            val output = File(target)
+
+            val lines = input.readLines()
+            val mappedLines = lines.map { line ->
+                val numbers = line.split(" ").mapNotNull { it.toIntOrNull() }
+                if (numbers.isNotEmpty()) numbers.sum() / numbers.size else 0
+            }
+
+            output.writeText(mappedLines.joinToString("\n"))
+        } catch (e: FileNotFoundException) {
+            println("Файл не обнаружен: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 
     /**
@@ -21,6 +45,24 @@ object Average {
      * @param target путь до файла с результатами.
      */
     fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            val input = Paths.get(source)
+            val output = Paths.get(target)
+
+            val lines = Files.readAllLines(input)
+            val mappedLines = lines.map { line ->
+                val numbers = line.split(" ").mapNotNull { it.toIntOrNull() }
+                if (numbers.isNotEmpty()) numbers.sum() / numbers.size else 0
+            }
+
+            Files.write(output, mappedLines.joinToString("\n").toByteArray())
+        } catch (e: FileNotFoundException) {
+            println("Файл не обнаружен: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
+
 }
