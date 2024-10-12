d1299a07f830a895d93baa2a50c5b5b515d25e7c
NikSne <commits@niksne.ru>
2024-10-12 09:23:50 +0300

    initial commit

--- lesson4/src/main/kotlin/Average.kt
+++ lesson4/src/main/kotlin/Average.kt
@@ -1,22 +1,26 @@
 package ru.tbank.education.school
 
-/**
- * Класс для вычисления среднего арифметического.
- */
+import java.io.File
+
+/** Класс для вычисления среднего арифметического. */
 object Average {
     /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java IO.
+     * Метод обрабатывает исходный файл и записывает результат в файл с результатами. Обработка
+     * выполняется через Java IO.
      * @param source путь до исходного файла.
      * @param target путь до файла с результатами.
      */
     fun processFileIO(source: String, target: String) {
-        TODO()
+        val inputFile = File(javaClass.classLoader.getResource(source)!!.file)
+        val outputFile = File(javaClass.classLoader.getResource(target)!!.file)
+        for (test in inputFile.readLines()) {
+            println(test)
+        }
     }
 
     /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java NIO.
+     * Метод обрабатывает исходный файл и записывает результат в файл с результатами. Обработка
+     * выполняется через Java NIO.
      * @param source путь до исходного файла.
      * @param target путь до файла с результатами.
      */
