a0aa8fd6a47e652cf690cce7738fcd607b7dd9b7
zaharzinin <zaharzinin@gmail.com>
2024-10-13 22:21:00 +0300

    Легчайшая

--- lesson4/src/main/kotlin/Analyzer.kt
+++ lesson4/src/main/kotlin/Analyzer.kt
@@ -1,5 +1,9 @@
 package ru.tbank.education.school
-
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 /**
  * Класс для анализа содержимого файла.
  */
@@ -11,7 +15,41 @@ object Analyzer {
      * @param target путь до файла с результатами.
      */
     fun processFileIO(source: String, target: String) {
-        TODO()
+        try {
+            var inputfileio = File(source)
+            var listav: MutableList<Double> = mutableListOf()
+            var lines = inputfileio.readLines()
+            var g = 0
+            var l: MutableList<String> = mutableListOf()
+            var t = 0
+            listav.add(lines.size.toDouble())
+            for (i in lines) {
+                g = g + i.split(" ").size
+            }
+            listav.add(g.toDouble())
+            for (i in lines) {
+                for (i2 in i.split(" ")) {
+                    l.add(i2)
+                }
+            }
+            listav.add(l.toSet().size.toDouble())
+            for (i in l) {
+                t = t + i.length
+            }
+            var u: Double = t / (l.size).toDouble()
+            listav.add(u)
+            var outputio = File(target)
+            outputio.writeText("Общее количество строк: ${listav[0]}\n")
+            outputio.writeText("Общее количество слов: ${listav[1]}\n")
+            outputio.writeText("Уникальные слова: ${listav[2]}\n")
+            outputio.writeText("Средняя длина слов: ${listav[3]}\n")
+        } catch (e: FileNotFoundException) {
+        println("Файл не обнаружен: ${e.message}")
+    } catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        } catch (e: Exception) {
+        println("Неизвестная ошибка: ${e.message}")
+    }
     }
 
     /**
@@ -21,6 +59,40 @@ object Analyzer {
      * @param target путь до файла с результатами.
      */
     fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            var inputfileio = Paths.get(source)
+            var listav: MutableList<Double> = mutableListOf()
+            var lines = Files.readAllLines(inputfileio)
+            var g = 0
+            var l: MutableList<String> = mutableListOf()
+            var t = 0
+            listav.add(lines.size.toDouble())
+            for (i in lines) {
+                g = g + i.split(" ").size
+            }
+            listav.add(g.toDouble())
+            for (i in lines) {
+                for (i2 in i.split(" ")) {
+                    l.add(i2)
+                }
+            }
+            listav.add(l.toSet().size.toDouble())
+            for (i in l) {
+                t = t + i.length
+            }
+            var u: Double = t / (l.size).toDouble()
+            listav.add(u)
+            var outputio = File(target)
+            outputio.appendText("Общее количество строк: ${listav[0]}\n")
+            outputio.appendText("Общее количество слов: ${listav[1]}\n")
+            outputio.appendText("Уникальные слова: ${listav[2]}\n")
+            outputio.appendText("Средняя длина слов: ${listav[3]}\n")
+        } catch (e: FileNotFoundException) {
+            println("Файл не обнаружен: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 }
--- lesson4/src/main/kotlin/Average.kt
+++ lesson4/src/main/kotlin/Average.kt
@@ -1,8 +1,12 @@
 package ru.tbank.education.school
 
-/**
- * Класс для вычисления среднего арифметического.
- */
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Paths
+import kotlin.io.path.appendText
+import java.nio.file.Files
+
 object Average {
     /**
      * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
@@ -11,16 +15,53 @@ object Average {
      * @param target путь до файла с результатами.
      */
     fun processFileIO(source: String, target: String) {
-        TODO()
-    }
-
-    /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java NIO.
-     * @param source путь до исходного файла.
-     * @param target путь до файла с результатами.
-     */
-    fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            var inputfileio = File(source)
+            var listav: MutableList<Double> = mutableListOf()
+            var lines = inputfileio.readLines()
+            val outputfileio = File(target)
+            for (line in lines) {
+                var line1 = line.split(" ").map { it.toInt() }
+                var t = line1
+                listav.add(t.average())
+            }
+            outputfileio.writeText(listav.joinToString(" "))
+        } catch (e: FileNotFoundException) {
+            println("Высосо, файл не найден ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 }
+
+/**
+ * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
+ * Обработка выполняется через Java NIO.
+ * @param source путь до исходного файла.
+ * @param target путь до файла с результатами.
+ */
+fun processFileNIO(source: String, target: String) {
+    try {
+        var inputnio = Paths.get(source)
+        var listav: MutableList<Double> = mutableListOf()
+        val lines = Files.readAllLines(inputnio)
+        for (line in lines) {
+            var line1 = line.split(" ").map {it.toInt()}
+            var t = line1
+            listav.add(t.average())
+        }
+        var outputnio = Paths.get(target)
+        outputnio.appendText(listav.joinToString(" "))
+    } catch (e: FileNotFoundException) {
+        println("ЛОл, а такого файла нету:${e.message}")
+    } catch (e: FileNotFoundException) {
+        println("Ошибка при работе с файлами:${e.message}")
+    } catch (e: FileNotFoundException) {
+        println("Неизвестная ошибка:${e.message}")
+    }
+}
+
+
+
