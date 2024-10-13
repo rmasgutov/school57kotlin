4bf710bccc77c8cdc243dc85c6b6acda4bdcf31a
rafbig <raf.big8@gmail.com>
2024-10-13 21:58:54 +0300

    Биглов Рафаэль lesson 4

--- lesson4/src/main/kotlin/Average.kt
+++ lesson4/src/main/kotlin/Average.kt
@@ -1,5 +1,11 @@
 package ru.tbank.education.school
 
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+
 /**
  * Класс для вычисления среднего арифметического.
  */
@@ -11,7 +17,29 @@ object Average {
      * @param target путь до файла с результатами.
      */
     fun processFileIO(source: String, target: String) {
-        TODO()
+        try {
+            val inputFileIo = File(source)
+            val outputFileIo = File(target)
+
+            val lines = inputFileIo.readLines()
+            val listAverage: MutableList<Double> = mutableListOf()
+            for (line in lines) {
+                val numbers = line.split(" ").mapNotNull { it.toIntOrNull() }
+                if (numbers.isNotEmpty()) {
+                    listAverage.add(numbers.average())
+                }
+                else {
+                    listAverage.add(0.0)
+                }
+            }
+            outputFileIo.writeText(listAverage.joinToString(" "))
+        } catch (e: FileNotFoundException) {
+            println("Файл не найден: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 
     /**
@@ -21,6 +49,31 @@ object Average {
      * @param target путь до файла с результатами.
      */
     fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            val inputFileNIO = Paths.get(source)
+            val outputFileNIO = Paths.get(target)
+
+            val listAverage: MutableList<Double> = mutableListOf()
+            val lines = Files.readAllLines(inputFileNIO)
+            for (line in lines) {
+                val numbers = line.split(" ").mapNotNull { it.toIntOrNull() }
+                if (numbers.isNotEmpty()) {
+                    listAverage.add(numbers.average())
+                }
+                else {
+                    listAverage.add(0.0)
+                }
+            }
+            Files.write(outputFileNIO, listAverage.joinToString(" ").toByteArray())
+        }
+        catch (e: java.nio.file.NoSuchFileException) {
+            println("Файл не найден: ${e.message}")
+        }
+        catch (e: IOException) {
+            println("Ошибка при работе с файлами: ${e.message}")
+        }
+        catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 }
