5993ccad5db88a9be94854d81dcb52fac8aa7f76
maplerain42 <kolesnikova.09.04.09@gmail.com>
2024-10-19 00:49:35 +0300

    Колесникова Алёна урок 4

--- lesson4/src/main/kotlin/Average.kt
+++ lesson4/src/main/kotlin/Average.kt
@@ -4,6 +4,7 @@ import java.io.File
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
+
 object Average {
     fun processFileIO(source: String, target: String) {
         try{
