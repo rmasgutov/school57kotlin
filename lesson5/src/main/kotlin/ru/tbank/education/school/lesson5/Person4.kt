d43947f13d716b315944e9b5f6942421b78257ba
maplerain42 <kolesnikova.09.04.09@gmail.com>
2024-10-19 10:58:19 +0300

    Колесникова Алёна урок 5

--- lesson5/src/main/kotlin/ru/tbank/education/school/lesson5/Person4.kt
+++ lesson5/src/main/kotlin/ru/tbank/education/school/lesson5/Person4.kt
@@ -2,7 +2,9 @@ package ru.tbank.education.school.ru.tbank.education.school.lesson5
 
 import java.time.LocalDate
 import java.util.*
+import com.fasterxml.jackson.annotation.JsonInclude
 
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
 data class Person4(
     val firstName: String,
     val lastName: String,
