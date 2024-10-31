c8ed4c27ac8dad537d143b1dcd98adc32931f867
ilyat <ilyatirex@gmail.com>
2024-10-31 16:31:33 +0300

    16:31.31.10.24

--- lesson4/src/main/kotlin/Analyzer.kt
+++ lesson4/src/main/kotlin/Analyzer.kt
@@ -1,5 +1,13 @@
 package ru.tbank.education.school
 
+
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
 /**
  * Класс для анализа содержимого файла.
  */
@@ -11,7 +19,35 @@ object Analyzer {
      * @param target путь до файла с результатами.
      */
     fun processFileIO(source: String, target: String) {
-        TODO()
+        try {
+            val inputFileIO = File(source)
+            val outputFileIO = File(target)
+            val lines = inputFileIO.readLines()
+            var words = 0
+            var totalLenth = 0.0
+            var cntOfLines = 0
+            val uniqueWords = mutableSetOf<String>()
+            for (line in lines) {
+                words += line.split(" ").size
+                cntOfLines++
+                uniqueWords.addAll(line.split(" "))
+                for (word in line.split(" ")) totalLenth += word.length
+            }
+            val result = """
+                Общее количество строк: $cntOfLines
+                Общее количество слов: $words
+                Уникальные слова: ${uniqueWords.size}
+                Средняя длина слов: ${totalLenth / words}
+            """.trimIndent()
+            outputFileIO.writeText(result)
+        } catch (e: FileNotFoundException) {
+            println("Такого файла не существует:\n${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлом(и):\n${e.message}")
+        }
+        catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 
     /**
@@ -21,6 +57,34 @@ object Analyzer {
      * @param target путь до файла с результатами.
      */
     fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            val inputFileNIO = Paths.get(source)
+            val outputFileNIO = Paths.get(target)
+            val lines = Files.readAllLines(inputFileNIO)
+            var words = 0
+            var totalLenth = 0.0
+            var cntOfLines = 0
+            val uniqueWords = mutableSetOf<String>()
+            for (line in lines) {
+                words += line.split(" ").size
+                cntOfLines++
+                uniqueWords.addAll(line.split(" "))
+                for (word in line.split(" ")) totalLenth += word.length
+            }
+            val result = """
+                Общее количество строк: $cntOfLines
+                Общее количество слов: $words
+                Уникальные слова: ${uniqueWords.size}
+                Средняя длина слов: ${totalLenth / words}
+            """.trimIndent()
+            Files.write(outputFileNIO, result.toByteArray())
+        } catch (e: FileNotFoundException) {
+            println("Такого файла не существует:\n${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлом(и):\n${e.message}")
+        }
+        catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 }
--- lesson4/src/main/kotlin/Average.kt
+++ lesson4/src/main/kotlin/Average.kt
@@ -1,5 +1,11 @@
 package ru.tbank.education.school
 
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
 /**
  * Класс для вычисления среднего арифметического.
  */
@@ -11,7 +17,32 @@ object Average {
      * @param target путь до файла с результатами.
      */
     fun processFileIO(source: String, target: String) {
-        TODO()
+        try {
+            val inputFileIO = File(source)
+            val outputFileIO = File(target)
+            val lines = inputFileIO.readLines()
+            var arithmeticMean = ""
+
+            for (lien in lines) {
+                val numbers = lien.split(" ")
+
+                val sum = numbers.map { it.toDouble() }.sum()
+                val count = numbers.size
+
+                val average = if (count > 0) sum / count else 0.0
+                arithmeticMean += average.toString() + " "
+            }
+            outputFileIO.writeText(arithmeticMean)
+
+        } catch (e: FileNotFoundException) {
+            println("Такого файла не существует:\n${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлом(и):\n${e.message}")
+        }
+        catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
+
     }
 
     /**
@@ -21,6 +52,25 @@ object Average {
      * @param target путь до файла с результатами.
      */
     fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            val inputFileNIO = Paths.get(source)
+            val outputFileNIO = Paths.get(target)
+            val lines = Files.readAllLines(inputFileNIO)
+
+            var arithmeticMean = lines.map { line ->
+                val numbers = line.split(" ").map { it.toInt() }
+                if (numbers.isNotEmpty()) numbers.sum() / numbers.size
+                else 0
+            }
+            Files.write(outputFileNIO, arithmeticMean.joinToString("\n").toByteArray())
+
+        } catch (e: FileNotFoundException) {
+            println("Такого файла не существует:\n${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка при работе с файлом(и):\n${e.message}")
+        }
+        catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 }
