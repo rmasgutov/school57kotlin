362dc79a914081fde45626ff073915454955f5cc
Ashot_27 <zvyagintsev208@gmail.com>
2024-11-08 23:02:21 +0300

    hometask

--- lesson6/src/main/kotlin/ru/tbank/education/school/lesson6/client/feign/user/UserApi.kt
+++ lesson6/src/main/kotlin/ru/tbank/education/school/lesson6/client/feign/user/UserApi.kt
@@ -8,7 +8,7 @@ import ru.tbank.education.school.lesson6.client.dto.User
 interface UserApi {
     @RequestLine("PUT /v2/user/{username}")
     @Headers("Content-Type: application/json")
-    fun updateUser(user: String, user1: User): ApiResponse
+    fun updateUser(@Param("username") user: String, user1: User): ApiResponse
 
     @RequestLine("POST /v2/user")
     @Headers("Content-Type: application/json")
