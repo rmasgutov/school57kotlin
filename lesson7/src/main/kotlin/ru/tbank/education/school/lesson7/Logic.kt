ceb1272f8c7986a1c1ffaebed7e07238ada9e65a
Maria Nepomyaschaya <marianepom555@gmail.com>
2024-11-09 11:00:00 +0300

    lesson 4

--- lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
+++ lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
@@ -7,14 +7,14 @@ import kotlin.math.pow
 /**
  * Текущий баланс по вкладу, отображающийся в личном кабинете с учетом всех начислений.
  */
-fun currentBalance(deposit: Deposit) = deposit.initialDeposit + (
+fun currentBalance(deposit: Deposit) = deposit.initialDeposit * (
     1 + (
         deposit.`сложная процентная ставка`.let {
-            if (deposit.isVip == 1) {
-                it + 1
+            if (deposit.isVip) {
+                deposit.it + 1
             } else {
-                it
+                deposit.it
             }
         }
-        ) / 356
-    ).pow(Duration.between(deposit.createAt, LocalDate.now()).toDays().toDouble())
+        ) / 100
+    ).pow(min(Duration.between(deposit.createAt, LocalDate.now()).toDays().toDouble(), deposit.durationMonth * 30))
