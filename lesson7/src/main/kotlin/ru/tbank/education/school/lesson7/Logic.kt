180010657723eb2801c7a71300010ae77457d6ab
Dan <76772649+physcorgi@users.noreply.github.com>
2024-11-14 21:07:39 +0300

    Update Logic.kt

--- lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
+++ lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
@@ -1,4 +1,4 @@
-import ru.tbank.education.deposits.Deposit
+import ru.tbank.education.school.lesson7.Deposit
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 import kotlin.math.pow
@@ -7,13 +7,13 @@ import kotlin.math.pow
  * Текущий баланс по вкладу, отображающийся в личном кабинете с учетом всех начислений.
  */
 fun currentBalance(deposit: Deposit) = deposit.initialDeposit * (
-        1 + (
-                deposit.compoundInterestRate.let {
-                    if (deposit.isVip) {
-                        it + 1
-                    } else {
-                        it
-                    }/100
-                }
-                ) / 365.0//не правильное колчество дней
-        ).pow(ChronoUnit.DAYS.between(deposit.createAt, LocalDate.now()).toDouble())
+    1 + (
+        deposit.compoundInterestRate.let {
+            if (deposit.isVip) {
+                it + 1
+            } else {
+                it
+            } / 100 // корректное количество дней
+        }
+    ) / 365.0
+).pow(ChronoUnit.DAYS.between(deposit.createAt, LocalDate.now()).toDouble())
