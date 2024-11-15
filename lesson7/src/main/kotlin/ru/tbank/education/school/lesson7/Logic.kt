521ed904c72c5c3889d3be742bb1c9b3e1897321
NikSne <commits@niksne.ru>
2024-11-15 22:26:10 +0300

    feat: comlete hw 7

--- lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
+++ lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
@@ -5,16 +5,27 @@ import java.time.LocalDate
 import kotlin.math.pow
 
 /**
- * Текущий баланс по вкладу, отображающийся в личном кабинете с учетом всех начислений.
+ * Количество дней в году
  */
-fun currentBalance(deposit: Deposit) = deposit.initialDeposit + (
-    1 + (
-        deposit.`сложная процентная ставка`.let {
-            if (deposit.isVip == 1) {
-                it + 1
-            } else {
-                it
-            }
-        }
-        ) / 356
-    ).pow(Duration.between(deposit.createAt, LocalDate.now()).toDays().toDouble())
+const val DAYS_IN_A_YEAR = 365
+
+/** Текущий баланс по вкладу, отображающийся в личном кабинете с учетом всех начислений. */
+fun currentBalance(deposit: Deposit) =
+    deposit.initialDeposit +
+        (
+            1 +
+                (
+                    deposit.interestRate.let {
+                        if (deposit.isVip == 1) {
+                            it + 1
+                        } else {
+                            it
+                        }
+                    }
+                    ) / DAYS_IN_A_YEAR
+            )
+            .pow(
+                Duration.between(deposit.createAt, LocalDate.now())
+                    .toDays()
+                    .toDouble()
+            )
--- lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Main.kt
+++ lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Main.kt
@@ -6,3 +6,4 @@ fun main() {
     val deposit = Deposit(1234566789, 10000, LocalDate.parse("2024-01-01"), 7, 20.0, 1)
     println(currentBalance(deposit))
 }
+
