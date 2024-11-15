d2ee970e5c805af562deeb2a6cba6b8154247a8a
matveiminervini <matvei.minervini@gmail.com>
2024-11-16 01:11:17 +0300

    ДЗ Минервини Матвей

--- lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
+++ lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Logic.kt
@@ -3,11 +3,11 @@ package ru.tbank.education.school.lesson7
 import java.time.Duration
 import java.time.LocalDate
 import kotlin.math.pow
-
+const val YEAR_DAYS = 365
 /**
  * Текущий баланс по вкладу, отображающийся в личном кабинете с учетом всех начислений.
  */
-const val YearDays = 365
+
 fun currentBalance(deposit: Deposit) = deposit.initialDeposit + (
-    1 + (deposit.complexPercent.let { if (deposit.isVip == 1) { it + 1 } else { it } }) / YearDays
+1 + (deposit.complexPercent.let { if (deposit.isVip == 1) { it + 1 } else { it } }) / YEAR_DAYS
     ).pow(Duration.between(deposit.createAt, LocalDate.now()).toDays().toDouble())
