59181e7d6435716107245cf097b5d97da1d1f13e
Krupnov <pavelkm10@gmail.com>
2024-11-27 19:52:55 +0300

    lesson9 Крупнов Павел

--- lesson9/src/main/kotlin/demo/application/controller/ScoreController.kt
+++ lesson9/src/main/kotlin/demo/application/controller/ScoreController.kt
@@ -1,17 +1,17 @@
 package demo.application.controller
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
-
+import demo.application.controller.LogicService
+import org.springframework.web.bind.annotation.PostMapping
 
 @RestController
 class ScoreController(
-    val logicService: LogicService,
+    val logicService: LogicService = LogicService(),
 ) {
 
-    @GetMapping("score")
+    @PostMapping("score")
     fun simpleScore(@RequestParam creditApplication: CreditApplication) =
         logicService.simpleScore(creditApplication)
 
@@ -38,4 +38,4 @@ data class CreditApplication(
     val totalAmount: Long,
     val monthlyPayment: Long,
     val user: User,
-    )
+)
