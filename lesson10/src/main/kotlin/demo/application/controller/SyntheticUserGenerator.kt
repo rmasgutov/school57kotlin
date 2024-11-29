fec0af80698e848e65a424b39da0236d7c05921f
tim <t.p.maerov@tinkoff.ru>
2024-11-30 00:46:36 +0300

    10 урок

--- lesson10/src/main/kotlin/demo/application/controller/SyntheticUserGenerator.kt
+++ lesson10/src/main/kotlin/demo/application/controller/SyntheticUserGenerator.kt
@@ -1,11 +1,9 @@
 package demo.application.controller
 
 import demo.application.dto.User
-import org.springframework.stereotype.Component
 import kotlin.random.Random
 
 // Доступ до CRM возможен только с продового сервера. На тесте используем сгенеренных пользователей
-@Component
 class SyntheticUserGenerator {
 
     fun generateUser() = User(
