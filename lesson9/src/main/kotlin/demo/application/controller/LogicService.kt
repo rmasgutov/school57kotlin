c58ba911c08c91e8404bd9372ed52c7b42699072
Maria Nepomyaschaya <marianepom555@gmail.com>
2024-11-29 21:21:25 +0300

    lesson 9

--- lesson9/src/main/kotlin/demo/application/controller/LogicService.kt
+++ lesson9/src/main/kotlin/demo/application/controller/LogicService.kt
@@ -3,7 +3,7 @@ package demo.application.controller
 class LogicService {
     fun simpleScore(creditApplication: CreditApplication): Boolean {
         // Нельзя выдавать кредит клиентам не достигшим 18 лет
-        if (creditApplication.user.age <=  18 ) {
+        if (creditApplication.user.age < 18 ) {
             throw RuntimeException("Клиент слишком мал")
         }
         // Если суммарный месячный платеж составляет больше трети дохода то нельзя выдавать новый кредит
--- lesson9/src/test/kotlin/demo/application/DemoApplicationTests.kt
+++ lesson9/src/test/kotlin/demo/application/DemoApplicationTests.kt
@@ -2,10 +2,12 @@ package demo.application
 
 import demo.application.controller.CreditApplication
 import demo.application.controller.ScoreController
+import demo.application.controller.User
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDateTime
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -14,17 +16,27 @@ class DemoApplicationTests {
 	lateinit var scoreController: ScoreController
 
 	@Test
-	fun someTest() {
-		val request = CreditApplication()
+	fun Test1() {
+		val request = CreditApplication(
+			createdAt = LocalDateTime.now(),
+			totalAmount = 100000,
+			monthlyPayment = 10000,
+			user = User(age = 30, name = "Victor", sex = 1, income = 200000, loans = listOf(User.Loan(LocalDateTime.now(), true, 10000)))
+		)
 		val result = scoreController.simpleScore(request)
 		Assertions.assertEquals(true, result)
 	}
 
 	@Test
-	fun otherTest() {
-		val request = CreditApplication()
+	fun Test2() {
+		val request = CreditApplication(
+			createdAt = LocalDateTime.now(),
+			totalAmount = 20000000000000000,
+			monthlyPayment = 10000,
+			user = User(age = 16, name = "Masha", sex = 2, income = 100000000000, loans = listOf(User.Loan(LocalDateTime.of(2022, 10, 12, 10, 30
+			), false, 100000)))
+		)
 		val result = scoreController.simpleScore(request)
 		Assertions.assertEquals(false, result)
 	}
-
 }
