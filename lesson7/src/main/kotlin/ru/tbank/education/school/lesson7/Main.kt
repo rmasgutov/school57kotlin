c0f68003504553b540377be7b9d1fc8f71e57ff5
Arina <makarovaas2008@gmail.com>
2024-12-21 00:24:18 +0300

    lesson 7

--- lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Main.kt
+++ lesson7/src/main/kotlin/ru/tbank/education/school/lesson7/Main.kt
@@ -1,8 +1,8 @@
 package ru.tbank.education.school.lesson7
 
+import currentBalance
 import java.time.LocalDate
-
 fun main() {
-    val deposit = Deposit(1234566789, 10000, LocalDate.parse("2024-01-01"), 7, 20.0, 1)
+    val deposit = Deposit(1234566789, 10000, LocalDate.parse("2024-01-01"), 7, 20.0, true)
     println(currentBalance(deposit))
 }
