1ae803590ebc96c421e6c8d84620f6b14a784039
vaceslavkusenko <SlavaKushch@yandex.ru>
2024-12-26 14:35:20 +0300

    Дз 4 урок

--- lesson4/src/main/kotlin/Average.kt
+++ lesson4/src/main/kotlin/Average.kt
@@ -1,26 +1,68 @@
 package ru.tbank.education.school
 
-/**
- * Класс для вычисления среднего арифметического.
- */
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
 object Average {
-    /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java IO.
-     * @param source путь до исходного файла.
-     * @param target путь до файла с результатами.
-     */
+
     fun processFileIO(source: String, target: String) {
-        TODO()
+        try {
+            val data = File(source)
+
+            val ans = File(target)
+
+            val lines = data.readLines()
+
+            val averages = countAverage(lines)
+
+            ans.writeText(averages.joinToString("\n"))
+        } catch (e: NumberFormatException) {
+            println("Ошибка формата данных: ${e.message}")
+        } catch (e: FileNotFoundException) {
+            println("Файл не найден: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка во время работы с файлом: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 
-    /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java NIO.
-     * @param source путь до исходного файла.
-     * @param target путь до файла с результатами.
-     */
+
     fun processFileNIO(source: String, target: String) {
-        TODO()
+        try {
+            val data = Paths.get(source)
+
+            val ans = Paths.get(target)
+
+            val lines = Files.readAllLines(data)
+
+            val averages = countAverage(lines)
+
+            Files.write(ans, averages.joinToString("\n").toByteArray())
+        } catch (e: NumberFormatException) {
+            println("Ошибка формата данных: ${e.message}")
+        } catch (e: FileNotFoundException) {
+            println("Файл не найден: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка во время работы с файлом: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
+
+    }
+    fun countAverage(lines: List<String>): List<Double> {
+        val averages = lines.map { line ->
+
+            if (line.isBlank()) 0.0
+            else {
+                val numbers = line.split("\\s+".toRegex()).map { it.toDouble() }
+                numbers.average()
+            }
+        }
+        return averages
     }
 }
