6eba40010af25012c5b93f97837f5d7f7b81312b
vaceslavkusenko <SlavaKushch@yandex.ru>
2024-12-27 06:19:20 +0300

    Дз 4 урок

--- lesson4/src/main/kotlin/Analyzer.kt
+++ lesson4/src/main/kotlin/Analyzer.kt
@@ -1,26 +1,82 @@
-package ru.tbank.education.school
+package org.example
 
-/**
- * Класс для анализа содержимого файла.
- */
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import kotlin.collections.mutableMapOf
+
+
 object Analyzer {
-    /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java IO.
-     * @param source путь до исходного файла.
-     * @param target путь до файла с результатами.
-     */
+
     fun processFileIO(source: String, target: String) {
-        TODO()
+        try {
+            val data = File(source)
+
+            val ans = File(target)
+
+            val lines = data.readLines()
+
+            val stats = countStats(lines)
+
+            ans.writeText("Общее количество строк: ${stats["rows"]}\n")
+            ans.appendText("Общее количество слов: ${stats["countWords"]}\n")
+            ans.appendText("Уникальных слов: ${stats["uniqueWords"]}\n")
+            ans.appendText("Средняя длина слов: ${stats["averageLength"]}\n")
+
+        } catch (e: FileNotFoundException) {
+            println("Файл не найден: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка во время работы с файлом: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
 
-    /**
-     * Метод обрабатывает исходный файл и записывает результат в файл с результатами.
-     * Обработка выполняется через Java NIO.
-     * @param source путь до исходного файла.
-     * @param target путь до файла с результатами.
-     */
+
     fun processFileNIO(source: String, target: String) {
-        TODO()
+
+        try {
+            val data = Paths.get(source)
+
+            val ans = Paths.get(target)
+
+            val lines = Files.readAllLines(data)
+
+            val stats = countStats(lines)
+
+            Files.write(ans, "Общее количество строк: ${stats["rows"]}\n".toByteArray())
+            Files.write(ans, "Общее количество слов: ${stats["countWords"]}\n".toByteArray(), StandardOpenOption.APPEND)
+            Files.write(ans, "Уникальных слов: ${stats["uniqueWords"]}\n".toByteArray(), StandardOpenOption.APPEND)
+            Files.write(ans, "Средняя длина слов: ${stats["averageLength"]}\n".toByteArray(), StandardOpenOption.APPEND)
+
+
+        } catch (e: NoSuchFileException) {
+            println("Файл не найден: ${e.message}")
+        } catch (e: IOException) {
+            println("Ошибка во время работы с файлом: ${e.message}")
+        } catch (e: Exception) {
+            println("Неизвестная ошибка: ${e.message}")
+        }
     }
+    private fun countStats(lines: List<String>): MutableMap <String, Int> {
+
+        val statistics = mutableMapOf <String, Int>()
+        val words = lines.flatMap {
+            it.split("\\s+".toRegex())
+                .map { word -> word.lowercase() }
+                .filter { word -> word.isNotEmpty() } // Фильтруем только непустые слова
+        }
+
+        println(words)
+        statistics["rows"] = lines.size
+        statistics["countWords"] = words.size
+        statistics["uniqueWords"] = words.toSet().size
+        statistics["averageLength"] = if (words.isNotEmpty()) words.sumOf { it.length } / words.size else 0
+        println(statistics)
+        return statistics
+    }
+
 }
