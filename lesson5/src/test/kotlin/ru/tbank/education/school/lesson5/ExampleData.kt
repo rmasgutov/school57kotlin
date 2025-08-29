9c18a17359657724217fd0953a250bfe9b110213
n.i.koshcheev <n.i.koshcheev@tinkoff.ru>
2025-08-29 09:23:11 +0300

    add content fot lesson5_2025

--- lesson5/src/test/kotlin/ru/tbank/education/school/lesson5/ExampleData.kt
+++
@@ -1,9 +0,0 @@
-package ru.tbank.education.school.lesson5
-
-data class ExampleData(
-    val firstName: String,
-    val lastName: String,
-    val middleName: String,
-    val age: Int,
-    val isHuman: Boolean,
-)
