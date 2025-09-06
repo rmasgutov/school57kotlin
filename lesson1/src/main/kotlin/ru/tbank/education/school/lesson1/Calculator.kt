b8e006feb152e5f8684589c0f66848c1a5bc34d4
ITQ <itq.dev@ya.ru>
2025-09-06 20:04:42 +0300

    feat(lesson1): implemented calculate function

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -3,8 +3,17 @@ package ru.tbank.education.school.lesson1
 /**
  * Метод для вычисления простых арифметических операций.
  */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+fun calculate(a: Double, b: Double, operation: OperationType): Double? = when (operation) {
+    OperationType.ADD -> a + b
+    OperationType.SUBTRACT -> a - b
+    OperationType.MULTIPLY -> a * b
+    OperationType.DIVIDE -> {
+        if (b == 0.0) {
+            null
+        } else {
+            a / b
+        }
+    }
 }
 
 /**
