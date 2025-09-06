98fe652a8ad151d64d508a84113cf85c22b52860
ITQ <itq.dev@ya.ru>
2025-09-06 18:31:25 +0300

    feat(lesson1): implemented even numbers sum function

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -4,5 +4,12 @@ package ru.tbank.education.school.lesson1
  * Сумма четных чисел.
  */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var sum = 0
+    for (number in numbers) {
+        if (number % 2 == 0) {
+            sum += number
+        }
+    }
+
+    return sum
 }
