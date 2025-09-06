9fca287bdc69a4302058fef98a165648fa209af8
meganikii <lysovniki@gmail.com>
2025-09-06 15:52:15 +0300

    test

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -2,7 +2,19 @@ package ru.tbank.education.school.lesson1
 
 /**
  * Сумма четных чисел.
- */
+ */ё
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var total = 0
+    for (number in numbers) {
+        if (number % 2 == 0) {
+            total += number
+        }
+    }
+    return total
+}
+
+
+fun main() {
+    val numbers = readLine()!!.split(" ").map { it.toInt() }.toTypedArray()
+    println(sumEvenNumbers(numbers))
 }
