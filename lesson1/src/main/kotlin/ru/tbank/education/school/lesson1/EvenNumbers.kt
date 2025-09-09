551203e013ffefd51a197b088163709e2da3874a
meganikii <lysovniki@gmail.com>
2025-09-09 20:44:05 +0300

    Basic Solution. Homework. Lysov

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -2,19 +2,15 @@ package ru.tbank.education.school.lesson1
 
 /**
  * Сумма четных чисел.
- */ё
+ */
 fun sumEvenNumbers(numbers: Array<Int>): Int {
     var total = 0
     for (number in numbers) {
-        if (number % 2 == 0) {
-            total += number
-        }
+        if (number % 2 == 0) total += number
     }
     return total
 }
 
-
 fun main() {
-    val numbers = readLine()!!.split(" ").map { it.toInt() }.toTypedArray()
-    println(sumEvenNumbers(numbers))
+    println(sumEvenNumbers(arrayOf(1, 2, 3, 4, 5)))
 }
