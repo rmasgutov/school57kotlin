a263f64815fc1a6df45010dd5dbfafe99cf19c6c
ITQ <itq.dev@ya.ru>
2025-09-11 19:53:49 +0300

    chore(lesson1): improvements in even numbers function

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -6,10 +6,15 @@ package ru.tbank.education.school.lesson1
 fun sumEvenNumbers(numbers: Array<Int>): Int {
     var sum = 0
     for (number in numbers) {
-        if (number % 2 == 0) {
-            sum += number
-        }
+        if (number % 2 != 0) continue
+        sum += number
     }
 
     return sum
+}
+
+fun main() {
+    val input = readlnOrNull()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+    val result = sumEvenNumbers(input)
+    println(result)
 }
