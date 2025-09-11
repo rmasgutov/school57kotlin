0ae9bb633dc354865d97a137b00227f237eac031
Elena Ponomareva <eponomar9@gmail.com>
2025-09-12 00:20:20 +0300

    Lesson 1

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,18 +1,52 @@
 package ru.tbank.education.school.lesson1
+import kotlin.math.sqrt
+import kotlin.math.sin
+import kotlin.math.cos
 
-/**
- * Метод для вычисления простых арифметических операций.
- */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    return when(operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> if (b == 0.0) null else a / b
+        OperationType.SQRT -> sqrt(a)
+        OperationType.SIN -> sin(a)
+        OperationType.COS -> cos(a)
+    }
 }
 
-/**
- * Функция вычисления выражения, представленного строкой
- * @return результат вычисления строки или null, если вычисление невозможно
- * @sample "5 * 2".calculate()
- */
+fun detectOperation(op: String): OperationType? {
+    when(op) {
+        "+" -> return OperationType.ADD
+        "-" -> return OperationType.SUBTRACT
+        "*" -> return OperationType.MULTIPLY
+        "/" -> return OperationType.DIVIDE
+        "sin" -> return OperationType.SIN
+        "cos" -> return OperationType.COS
+        "sqrt" -> return OperationType.SQRT
+    }
+    return null
+}
+
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val s = this.split(" ")
+    if (s.size < 2 || s.size > 3) { return null }
+    if (s.size == 2) {
+        val op = detectOperation(s[0]) ?: return s[0].toDouble() + s[1].toDouble()
+        val a = s[1].toDoubleOrNull() ?: return null
+        return calculate(a, 0.0, op)
+    }
+
+    val a = s[0].toDoubleOrNull() ?: return null
+    val b = s[2].toDoubleOrNull() ?: return null
+    val op = detectOperation(s[1]) ?: return null
+
+    return calculate(a, b, op)
+}
+
+fun main() {
+    var s = readln()
+    s.calculate()?.let { println(it) }
+    //println(s.calculate())
 }
--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -1,8 +1,18 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Сумма четных чисел.
- */
-fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+fun sumEvenNumbers(mas: Array<Int>): Int {
+    var ans = 0
+    for (el in mas) {
+        if (el % 2 == 0) {
+            ans += el
+        }
+    }
+    return ans
+}
+
+fun main() {
+    println("Input something:")
+    val mass = readln()
+    val mas = mass.split(" ").map { it.toInt() }.toTypedArray()
+    println(sumEvenNumbers(mas))
 }
