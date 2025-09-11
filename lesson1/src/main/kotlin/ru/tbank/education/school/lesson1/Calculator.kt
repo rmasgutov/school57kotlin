47a62e7804320e96da61f85702563b0cb2fe982e
ITQ <itq.dev@ya.ru>
2025-09-11 20:44:06 +0300

    feat(lesson1): added fun overload and extension function for string

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,20 +1,26 @@
 package ru.tbank.education.school.lesson1
 
+import java.math.BigDecimal
+
 /**
  * Метод для вычисления простых арифметических операций.
  */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? = when (operation) {
-    OperationType.ADD -> a + b
-    OperationType.SUBTRACT -> a - b
-    OperationType.MULTIPLY -> a * b
-    OperationType.DIVIDE -> {
-        if (b == 0.0) {
-            null
-        } else {
-            a / b
+fun calculate(a: BigDecimal, b: BigDecimal, operation: OperationType = OperationType.ADD): BigDecimal? =
+    when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> {
+            if (b == BigDecimal(0.0)) {
+                null
+            } else {
+                a / b
+            }
         }
     }
-}
+
+fun calculate(a: Double, b: Double, operation: OperationType = OperationType.ADD): Double? =
+    calculate(BigDecimal(a), BigDecimal(b), operation)?.toDouble()
 
 /**
  * Функция вычисления выражения, представленного строкой
@@ -23,5 +29,27 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? = when (o
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val tokens = this.split(" ")
+    if (tokens.size != 3) return null
+    val a = tokens[0].toDoubleOrNull() ?: return null
+    val b = tokens[2].toDoubleOrNull() ?: return null
+    val operation = when (tokens[1]) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> return null
+    }
+
+    return calculate(a, b, operation)
+}
+
+fun main() {
+    val res = readlnOrNull()?.calculate()
+
+    if (res == null) {
+        println("Ошибка")
+    } else {
+        println("Результат: $res")
+    }
 }
