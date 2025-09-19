84b62594290ac227ca676d8c394505fd0bd6da8a
ITQ <itq.dev@ya.ru>
2025-09-19 19:44:50 +0300

    feat(lesson2): added demo usage of DemoList class

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/Main.kt
@@ -3,9 +3,14 @@ package ru.tbank.education.school.lesson2
 fun main() {
     val list = DemoList.createList()
     println("Original list: $list")
-    TODO()
+    println("Sum: ${DemoList.sumList(list)}")
+    println("Even sum: ${DemoList.sumEvenList(list)}")
+    println("Max element: ${DemoList.maxElement(list)}")
+    println()
+    println("Multiplied list: ${DemoList.multiplyList(list)}")
+    println("Sorted list (DESC): ${DemoList.sortDesc(list)}")
 
-    val map = DemoMap.createMap()
-    println("Original map: $map")
-    TODO()
+//    val map = DemoMap.createMap()
+//    println("Original map: $map")
+//    TODO()
 }
