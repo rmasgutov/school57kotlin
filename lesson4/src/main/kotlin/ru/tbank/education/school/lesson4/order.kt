b2ac2b36525095205a653c33363dd8205abc9d1e
DimCher10 <103523226+DimCher10@users.noreply.github.com>
2025-10-03 01:22:30 +0300

    Interface2.kt

---
+++ lesson4/src/main/kotlin/ru/tbank/education/school/lesson4/order.kt
@@ -0,0 +1,33 @@
+interface Order {
+    val id: Int
+    val items: List<MenuItem>
+    val totalPrice: Double
+    fun addItem(item: MenuItem)
+}
+fun interface OrderNotifier {
+    @FunctionalInterface
+    fun notify(orderId: String, message: String)
+}
+interface OnlineOrder : Order {
+    val address: String
+    fun calculateDeliveryTime(distance: Double, averageSpeed: Double): Double
+}
+class OnlineOrder(
+    override val id: Int,
+    override val address: String) : OnlineOrder {
+    private val _items: MutableList<MenuItem> = mutableListOf()
+    
+    override val items: List<MenuItem>
+        get() = items.toList()
+    
+    override val totalPrice: Double
+        get() = items.sumOf { it.price }
+    
+    override fun addItem(item: MenuItem) {
+        items.add(item)
+        println("Добавлено: ${item.name} - ${item.price} руб.")
+    }
+    override fun calculateDeliveryTime(distance: Double, averageSpeed: Double): Double {
+        return (distance / averageSpeed) * 60 + 15
+    }
+}
