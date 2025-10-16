325c57c877d6fc9d5ca9fbcfc74e351d31804f5b
dimcher10 <st_d.p.chernyshev@ok654.ru>
2025-10-17 01:45:12 +0300

    homework lesson6

--- lesson6/src/main/kotlin/ru/tbank/education/school/lesson6/creditriskanalyzer/rules/HighRiskCategorySpendingRule.kt
+++ lesson6/src/main/kotlin/ru/tbank/education/school/lesson6/creditriskanalyzer/rules/HighRiskCategorySpendingRule.kt
@@ -1,8 +1,11 @@
 package ru.tbank.education.school.lesson6.creditriskanalyzer.rules
 
 import ru.tbank.education.school.lesson6.creditriskanalyzer.models.Client
+import ru.tbank.education.school.lesson6.creditriskanalyzer.models.PaymentRisk
 import ru.tbank.education.school.lesson6.creditriskanalyzer.models.ScoringResult
+import ru.tbank.education.school.lesson6.creditriskanalyzer.models.TransactionCategory
 import ru.tbank.education.school.lesson6.creditriskanalyzer.repositories.TransactionRepository
+
 
 /**
  * Анализирует долю расходов клиента в рисковых категориях.
@@ -26,6 +29,28 @@ class HighRiskCategorySpendingRule(
     override val ruleName: String = "High-Risk Category Spending"
 
     override fun evaluate(client: Client): ScoringResult {
-        TODO()
+
+        val transaction = transactionRepo.getTransactions(client.id)
+        var summi = 0L
+        var total = 0L
+        for (i in transaction) {
+            if (i.category == TransactionCategory.SALARY) {
+                continue
+            }
+            if (i.category == TransactionCategory.TRANSFER || i.category == TransactionCategory.GAMBLING || i.category == TransactionCategory.CRYPTO) {
+                summi += i.amount
+            }
+            total += i.amount
+        }
+        val risk = when {
+            total + summi == 0L -> PaymentRisk.LOW
+            5L * summi > 3L * total -> PaymentRisk.HIGH
+            10L * summi > 3L * total -> PaymentRisk.MEDIUM
+            else -> PaymentRisk.LOW
+        }
+        return ScoringResult(
+            ruleName,
+            risk
+        )
     }
 }
