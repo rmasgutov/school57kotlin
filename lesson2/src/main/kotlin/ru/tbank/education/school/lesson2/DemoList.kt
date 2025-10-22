a3d9bcec2ac256ca73ad26a440f05c118875209c
aDiamant03 <dva01082009@mail.ru>
2025-10-22 18:34:56 +0300

    dz 2 попытка отправить
    Signed-off-by: aDiamant03 <dva01082009@mail.ru>

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoList.kt
@@ -1,27 +1,46 @@
 package ru.tbank.education.school.lesson2
 
+import kotlin.random.Random
+
 object DemoList {
     fun createList(): List<Int> {
-        TODO()
+        return List(5) { Random.nextInt(1, 101) }
     }
 
     fun sumList(): Int {
-        TODO()
+        val list = createList()
+        println("Список: $list")
+        val sum = list.sum()
+        println("Сумма элементов: $sum")
+        return sum
     }
 
     fun sumEvenList(): Int {
-        TODO()
+        val list = createList()
+        println("Список: $list")
+        val sumEven = list.filter { it % 2 == 0 }.sum()
+        println("Сумма чётных элементов: $sumEven")
+        return sumEven
     }
 
     fun multiplyList(): List<Int> {
-        TODO()
+        val list = createList()
+        val multiplied = list.map { it * 2 }
+        println("Исходный: $list\nУмноженный: $multiplied")
+        return multiplied
     }
 
     fun maxElement(): Int {
-        TODO()
+        val list = createList()
+        val max = list.maxOrNull() ?: 0
+        println("Список: $list\nМаксимум: $max")
+        return max
     }
 
     fun sortDesc(): List<Int> {
-        TODO()
+        val list = createList()
+        val sorted = list.sortedDescending()
+        println("Исходный: $list\nПо убыванию: $sorted")
+        return sorted
     }
 }
--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/DemoMap.kt
@@ -1,23 +1,49 @@
 package ru.tbank.education.school.lesson2
 
+import kotlin.random.Random
+
 object DemoMap {
+
+    private val alphabet = ('a'..'z') + ('0'..'9')
+
+    private fun randomKey(): String {
+        val len = Random.nextInt(5, 11)
+        return buildString {
+            repeat(len) { append(alphabet.random()) }
+        }
+    }
+
     fun createMap(): Map<String, Int> {
-        TODO()
+        val map = List(5) { randomKey() to Random.nextInt(1, 101) }.toMap()
+        println("Карта: $map")
+        return map
     }
 
     fun maxValue(): Int {
-        TODO()
+        val map = createMap()
+        val max = map.values.maxOrNull() ?: 0
+        println("Максимальное значение: $max")
+        return max
     }
 
     fun keyForMaxValue(): String {
-        TODO()
+        val map = createMap()
+        val entry = map.maxByOrNull { it.value }
+        println("Ключ с максимальным значением: ${entry?.key}")
+        return entry?.key ?: ""
     }
 
     fun sortByValueDesc(): Map<String, Int> {
-        TODO()
+        val map = createMap()
+        val sorted = map.toList().sortedByDescending { it.second }.toMap()
+        println("Отсортировано по убыванию: $sorted")
+        return sorted
     }
 
     fun filterOddValues(): Map<String, Int> {
-        TODO()
+        val map = createMap()
+        val filtered = map.filter { (_, v) -> v % 2 != 0 }
+        println("Только нечётные значения: $filtered")
+        return filtered
     }
 }
