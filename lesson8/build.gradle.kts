fb5431ef82603647de7c35305b4aa7180baeb0f8
nikitakoshcheev <nikirofler@gmail.com>
2025-11-21 16:39:33 +0300

    lesson8 add content or practise

--- lesson8/build.gradle.kts
+++ lesson8/build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     id("buildlogic.kotlin-library-conventions")
+    id("jacoco")
 }
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+    }
 }
